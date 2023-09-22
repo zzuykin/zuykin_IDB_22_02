7424ac3c3e34c0002e58184d6003ab2a189b7e4c
zzuykin <145152148+zzuykin@users.noreply.github.com>
2023-09-22 13:21:51 +0300

    fromlesson 2

--- zuykin_IDB_22_02/Assets/Scripts/TestScript.cs
+++ zuykin_IDB_22_02/Assets/Scripts/TestScript.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FirstSript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject sphere;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sphere = GameObject.Find("Sphere");
+            Inst();
+        }
+    }
+
+    void Inst()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 mv = new Vector3(i, 0, 0);
+            Instantiate(sphere, mv, sphere.transform.rotation);
+        }
     }
 }
